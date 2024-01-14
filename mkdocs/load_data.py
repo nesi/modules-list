1bbf02c0bfaf8698854eef98db2ed84f91648c8b
cal <callum.walley@nesi.org.nz>
2024-01-15 09:28:12 +1300

    remove mkdocs

--- mkdocs/load_data.py
+++
@@ -1,9 +0,0 @@
-import mkdocs.plugins
-import json
-
-def on_env(env, config, files, **kwargs): 
-    env.globals["applications"]=json.load(open('module-list.json'))
-    # env.globals["domains"]=json.load(open('../tags/domains.json')).keys() # Needs list of cannon domains to make into
-
-    # for symbol, value in locals().items():
-    #     print(symbol, value)
